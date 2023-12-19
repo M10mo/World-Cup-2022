7363a2474f532664a42a99ca15652c46ae310f25
M10mo <mohdkanoongo@gmail.com>
2023-12-19 12:31:30 -0500

    button working

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-import './style.css'
-console.log("hello world");
-document.querySelector("#app").innerHTML += '<h1>Hello world</h1>';
+document.addEventListener("DOMContentLoaded", function () {
+  // Wait for DOM to load before adding event listeners
+  let detailsButton = document.getElementById('toggleDetails');
+  detailsButton.addEventListener('click', toggleDetails);
+});
+
+function toggleDetails() {
+  let detailsDiv = document.getElementById('details');
+  if (detailsDiv.style.display === 'none') {
+      detailsDiv.style.display = 'block';
+      document.getElementById('toggleDetails').textContent = 'Hide Details';
+  } else {
+      detailsDiv.style.display = 'none';
+      document.getElementById('toggleDetails').textContent = 'Show Details';
+  }
+}
